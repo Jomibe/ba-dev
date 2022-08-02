de8f47fcc69ca6201229713f25b009220dd63ed8
jberger <berger.jonasmichel@fh-swf.de>
2022-08-02 21:23:23 +0200

    adding api_reachable() to prepare()

--- src/preparing.py
+++ src/preparing.py
@@ -1,5 +1,7 @@
 """
+
 Diese Datei enthält Funktionen, welche für den ordnungsgemäßen Programmstart benötigt werden.
+
 """
 
 # Es gibt ein Problem mit der Erkennung von lokalen Modulen durch pylint. Daher:
@@ -12,9 +14,11 @@ Diese Datei enthält Funktionen, welche für den ordnungsgemäßen Programmstart
 # Eigene Imports
 from debugging import console
 from debugging import INFO, WARN, ERR, SUCC
+from telegram import api_reachable
 
 
-def prepare():
+def prepare(telegram_bot_token):
     console("Starte Vorbereitungen...", mode=INFO)
+    api_reachable(telegram_bot_token)
     console("Vorbereitungen erfolgreich abgeschlossen", mode=SUCC)
     return 0
