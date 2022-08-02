9ac97f1e448cf5c7612b4219e287abcb82c079ec
jberger <berger.jonasmichel@fh-swf.de>
2022-08-02 21:01:10 +0200

    Update constants.py

--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,14 @@
 """
+
 Enthält vom Programm verwendete statische Veriablen. Die Parameter können auf das eigene System angepasst werden.
+
 """
 
 # Zusätzliche Ausgaben zum Programmablauf ausgeben
 DEBUG = True
+
+# Wert in Sekunden, wie lange die "long poll" Vorgänge zum Abruf von Aktualisierungen der Telegram-API dauern sollen
+TELEGRAM_LONG_POLL_TIMEOUT = 30
+
+# ASCII-Textdatei, welche Informationen persistent auf dem Dateisystem speichert.
+STOREFILE = "store.txt"
