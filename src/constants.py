667209c367fda1a9596ba9bf153e045d2fe20e3a
jberger <berger.jonasmichel@fh-swf.de>
2022-08-02 20:25:48 +0200

    Create constants.py

---
+++ src/constants.py
@@ -0,0 +1,6 @@
+"""
+Enthält vom Programm verwendete statische Veriablen. Die Parameter können auf das eigene System angepasst werden.
+"""
+
+# Zusätzliche Ausgaben zum Programmablauf ausgeben
+DEBUG = True
