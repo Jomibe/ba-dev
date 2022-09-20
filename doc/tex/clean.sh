3b3a4b1dac57dff53d28de729b32b3abb25c885a
jberger <berger.jonasmichel@fh-swf.de>
2022-09-20 16:22:08 +0200

    Update clean.sh

--- doc/tex/clean.sh
+++ doc/tex/clean.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rm *.log *.aux *.bcf *.lof *.lol *.lot *.xml *.gz *.toc *.bbl *.blg *.synctex\(busy\)
+rm *.log *.aux *.bcf *.lof *.lol *.lot *.xml *.gz *.toc *.bbl *.blg *.synctex\(busy\) *.out
